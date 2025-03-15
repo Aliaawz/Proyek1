1538b9a931d86c0e052ecd96c365e9222a2af2dc
duapuluhsatu21 <nuraliaaa2109@gmail.com>
2025-03-15 13:11:29 +0700

    hmm gtmetrixnya masih B napa yakk

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-});
